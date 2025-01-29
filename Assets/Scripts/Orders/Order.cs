eecbdea17ff12b45e56da93df42ae20447d49c3a
Jake <jlariv95@gmail.com>
2025-01-29 16:29:32 -0500

    Reorganized file structure

---
+++ Assets/Scripts/Orders/Order.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Order : MonoBehaviour
+{
+    public static Action<Order> onOrderElapse;
+    public static Action<int> onSegmentComplete;
+    public static Action<Order> onOrderComplete;
+
+    [SerializeField] private Image _orderTimerBar;
+
+    private float _maxOrderTime;
+
+    private float _currentOrderTime;
+
+    private GameManager.ItemType _orderType;
+    private List<GameManager.ItemType> _orderSegments;
+    private int _orderSum;
+    private Transform _plate;
+    private TextMeshProUGUI _orderNumberText;
+
+    private int[] _timesByRarity = { 5, 10, 15, 20};
+    public int _orderID { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _orderSegments = new List<GameManager.ItemType>();
+        _plate = transform.GetChild(0);
+        _orderNumberText = transform.GetChild(2).GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+        int rarityNum = Random.Range(1, 5);
+        GameManager.ItemType orderType;
+        /*
+         * Rarity Chances:
+         * Common: 40%
+         * Uncommon: 30%
+         * Rare: 15%
+         * Legendary: 15%
+         */
+        for (int i = 0; i < rarityNum; i++)
+        {
+            int randIndex = Random.Range(1, 101);
+            if (randIndex <= 40)
+            {
+                orderType = GameManager.ItemType.SEAWEED;
+            }else if (randIndex <= 70)
+            {
+                orderType = GameManager.ItemType.KRILL;
+            }else if (randIndex <= 85)
+            {
+                orderType = GameManager.ItemType.SARDINE;
+            }
+            else
+            {
+                orderType = GameManager.ItemType.SQUID;
+            }
+            _plate.GetChild(i).GetComponent<Image>().sprite = GameManager.GetItemSprite(orderType);
+            _plate.GetChild(i).gameObject.SetActive(true);
+            _orderSegments.Add(orderType);
+        }
+
+        for (int i = rarityNum; i < 4; i++)
+        {
+            _plate.GetChild(i).gameObject.SetActive(false);
+        }
+
+        _orderSum = _orderSegments.Select(rarity => MoneyManager.costRarityTable[(int)rarity]).Sum();
+        _maxOrderTime = _orderSegments.Select(rarity => _timesByRarity[(int)rarity] + Random.Range(-3, 4)).Sum();
+        _currentOrderTime = _maxOrderTime;
+        // Update the order number when instantiated
+        UpdateOrderNumber(this);
+        // Update all order numbers when an order is completed or elapsed
+        onOrderComplete += UpdateOrderNumber;
+        onOrderElapse += UpdateOrderNumber;
+    }
+    void Update()
+    {
+        // Update the time an order has been active
+        // Update progress bar and delete when time reaches 0
+        _currentOrderTime -= Time.deltaTime;
+        _orderTimerBar.fillAmount = _currentOrderTime / _maxOrderTime;
+        if (_currentOrderTime <= 0)
+        {
+            Destroy(gameObject);
+            onOrderElapse?.Invoke(this);
+        }
+    }
+
+    private void UpdateOrderNumber(Order order)
+    {
+        // The order that is completed is destroyed so don't try to update it
+        if (GetSegments().Count == 0 || GetTimeRemaining() <= 0)
+        {
+            return;
+        }
+        Transform orderHolder = transform.parent;
+        for (int i = 0; i < orderHolder.childCount; i++)
+        {
+            if (gameObject.Equals(orderHolder.GetChild(i).gameObject))
+            {
+                // Orders are numbered 1-9, not 0-8
+                _orderNumberText.text = (i + 1).ToString();
+            }
+        }
+    }
+
+    public List<GameManager.ItemType> GetSegments()
+    {
+        return _orderSegments;
+    }
+
+    public float GetTimeRemaining()
+    {
+        return _currentOrderTime;
+    }
+
+    public float GetMaxTime()
+    {
+        return _maxOrderTime;
+    }
+
+    public int GetOrderSum()
+    {
+        return _orderSum;
+    }
+
+    // Remove the appropriate segment from the list and update its color when an order is processed
+    public void CompleteSegment(GameManager.ItemType type, int completingItem)
+    {
+        if (_orderSegments.Remove(type))
+        {
+            for (int i = 0; i < _plate.childCount; i++)
+            {
+                Image child = _plate.GetChild(i).GetComponent<Image>();
+                if (child.gameObject.activeSelf && child.sprite == GameManager.GetItemSprite(type))
+                {
+                    child.gameObject.SetActive(false);
+                    onSegmentComplete?.Invoke(completingItem);
+                    break;
+                }
+            }
+        }
+        // When there are no more segments the order is completed
+        if (_orderSegments.Count == 0)
+        {
+            onOrderComplete?.Invoke(this);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/MoneyManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+
+    public static Func<GameManager.ItemType, bool> canAffordItem;
+    public static Func<int, bool> canAffordPurchase;
+    public static int[] costRarityTable = { 5, 10, 15, 20 };
+    
+    [SerializeField] private TextMeshProUGUI _moneyText;
+    [SerializeField] private int _startingMoney = 100;
+    private int _money;
+    void Start()
+    {
+        _money = _startingMoney;
+        _moneyText.text = "$" + _money;
+        GameManager.purchaseItem += Purchase;
+        Order.onOrderComplete += SuccessfulOrder;
+        Refresh.onRefresh += RemoveMoney;
+        Item.onStoreItem += Purchase;
+        GameManager.addMoney += AddMoney;
+
+        canAffordItem += CanAfford;
+        canAffordPurchase += CanAfford;
+
+    }
+    
+    // Purchase an Item.
+    // Cost is determined by the costRarityTable
+    private void Purchase(GameManager.ItemType type)
+    {
+        int toSpend = costRarityTable[(int)type];
+        RemoveMoney(toSpend);
+    }
+    // Add money upon successful order based on the rarities needed to complete and the time remaining
+    private void SuccessfulOrder(Order order)
+    {
+        float timePercentRemaining = order.GetTimeRemaining() / order.GetMaxTime();
+        int toAdd = Mathf.CeilToInt(order.GetOrderSum() * GameStatsManager.GetTimeBonus(timePercentRemaining));
+        _money += toAdd;
+        _moneyText.text = "$" + _money;
+        GameStatsManager.onMoneyEarn?.Invoke(toAdd);
+    }
+    // Add money to the player's total and update the text
+    private void AddMoney(int amount)
+    {
+        _money += amount;
+        _moneyText.text = "$" + _money;
+        GameStatsManager.onMoneyEarn?.Invoke(amount);
+    }
+    // Remove money to the player's total and update the text
+    private void RemoveMoney(int amount)
+    {
+        _money -= amount;
+        _moneyText.text = "$" + _money;
+        GameStatsManager.onMoneySpend?.Invoke(amount);
+    }
+    
+    // Check if the player can afford to purchase something
+    private bool CanAfford(int amount)
+    {
+        return _money >= amount;
+    }
+    // Check if the player can afford to purchase an Item
+    private bool CanAfford(GameManager.ItemType type)
+    {
+        return CanAfford(costRarityTable[(int) type]);
+    }
+    
+    
+    
+}
